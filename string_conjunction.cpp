b56d04df799d3c1ac3df88f3eebf6a27070eedb0
L-I-R-I-X <kirillsorokotaga55@gmail.com>
2025-10-13 09:06:05 +0600

    Добавлена вторая улучшенная версия программы: инкапсуляция логики в классе BitString, добавление конструктора и методов для ввода, вывода и конъюнкции, улучшенная структура кода и сокращение количества кода.

--- string_conjunction.cpp
+++ string_conjunction.cpp
@@ -6,89 +6,66 @@
 using namespace std;
 
 const int SIZE = 8;
-const string inPromptTemplate = "Введите {N}-ую строку\n";
-const string outPromptTemplate = "{N}-ая строка (с нулями)";
-const string outPromptRes = "Результат";
 
-string inputString(int n) {
-    string prompt = inPromptTemplate;
-    // Находим маркер {N}
-    size_t pos = prompt.find("{N}");
-    if (pos != string::npos) {
-        // Заменяем {N} на номер строки
-        prompt.replace(pos, 3, to_string(n));
-    }
-    cout << prompt;
-    string input;
-    cin >> input;
-    return input;
-}
+class BitString {
+private:
+    string bs;
 
-bool checkString(const string& str) {
-    if (str.length() > SIZE) {
-        cout << "Длина строки не должна превышать " << SIZE << " символов!\n";
-        return false;
+    void fromString(const string& inputString) {
+        if (inputString.length() > SIZE) {
+            cout << "Длина строки не должна превышать " << SIZE << " символов!\n";
+            return;
+        }
+        for (char c : inputString) {
+            if (c != '0' && c != '1') {
+                cout << "Строка должна содержать только '0' и '1'.\n";
+                return;
+            }
+        }
+        bs = inputString;
+        bs.append(SIZE - inputString.length(), '0');
+        bs = bs.substr(0, SIZE);
     }
-    for (char c : str) {
-        if (c != '0' && c != '1') {
-            cout << "Строка должна содержать только '0' и '1'.\n";
-            return false;
+
+public:
+    BitString(const string& inputString = "") {
+        bs = string(SIZE, '0');
+        if (!inputString.empty()) {
+            fromString(inputString);
         }
     }
-    return true;
-}
 
-string aadZeros(const string& str) {
-    if (str.length() < SIZE) {
-        cout << "Длина строки менее " << SIZE << " символов\n";
-        cout << "Будет выполнено дополнение незначащими нулями\n";
-        int zeros_to_add = SIZE - str.length();
-        string zeros(zeros_to_add, '0');
-        return zeros + str;
+    void input(int n) {
+        string prompt = "Введите " + to_string(n) + "-ую строку\n";
+        cout << prompt;
+        string userInput;
+        cin >> userInput;
+        fromString(userInput);
     }
-    return str;
-}
 
-string conjunction(const string& str1, const string& str2) {
-    string result = "";
-    for (int i = 0; i < SIZE; ++i) {
-        result += (str1[i] == '1' && str2[i] == '1') ? '1' : '0';
+    void output(int n) {
+        string promptText = to_string(n) + "-ая строка (с нулями)\n";
+        cout << promptText;
+        cout << bs << "\n";
     }
-    return result;
-}
 
-void outputString(const string& str, int n) {
-    string prompt_text = outPromptTemplate;
-    size_t pos = prompt_text.find("{N}");
-    if (pos != string::npos) {
-        prompt_text.replace(pos, 3, to_string(n));
+    BitString conjunction(const BitString& other) const {
+        BitString result;
+        for (int i = 0; i < SIZE; ++i) {
+            result.bs[i] = (bs[i] == '1' && other.bs[i] == '1') ? '1' : '0';
+        }
+        return result;
     }
-    cout << prompt_text << "\n";
-    cout << str << "\n";
-}
-
-void outputRes(const string& res) {
-    cout << outPromptRes << ": " << res << "\n";
-}
+};
 
 int main() {
-    string temp1 = inputString(1);
-    while (!checkString(temp1)) {
-        temp1 = inputString(1);
-    }
-    string str1 = aadZeros(temp1);
-
-    string temp2 = inputString(2);
-    while (!checkString(temp2)) {
-        temp2 = inputString(2);
-    }
-    string str2 = aadZeros(temp2);
-
-    string res = conjunction(str1, str2);
-
-    outputString(str1, 1);
-    outputString(str2, 2);
-    outputRes(res);
-
+    BitString a, b;
+    a.input(1);
+    b.input(2);
+    BitString c = a.conjunction(b);
+    a.output(1);
+    b.output(2);
+    cout << "Результат: ";
+    c.output(3);
     return 0;
 }
